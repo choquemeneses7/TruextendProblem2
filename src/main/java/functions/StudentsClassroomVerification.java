f53378ef89ce5e9926c660b75bf597541d49c691
Veimar Choque <veimar.choque@truextend.com>
2020-07-24 22:18:39 -0400

    [develop] Add tests to verify the correct functionallity of program

--- src/main/java/functions/StudentsClassroomVerification.java
+++ src/main/java/functions/StudentsClassroomVerification.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class StudentsClassroomVerification {
 
-    public List<Element> studentsInClasses(List<Element> studentsList, List<Element> classroomsList){
+    public static List<Element> studentsInClasses(List<Element> studentsList, List<Element> classroomsList){
         List<Element> studentInClass = new ArrayList<Element>();
         for(Element student : studentsList) {
             for(Element classroom : classroomsList) {
@@ -20,23 +20,24 @@ public class StudentsClassroomVerification {
         return studentInClass;
     }
 
-    private boolean isStudentInClass(Element student, Element classroom) {
+    private static boolean isStudentInClass(Element student, Element classroom) {
         double studentLatitude = student.getLatitude();
         double studentLongitude = student.getLongitude();
         double classroomLatitude = classroom.getLatitude();
         double classroomLongitude = classroom.getLongitude();
-        final int earthRadius = 6371000;
+        final int earthRadius = 6367449;
 
-        double latitudeDistance = Math.toRadians(classroomLatitude - studentLatitude);
-        double longitudeDistance = Math.toRadians(classroomLongitude - studentLongitude);
-        double a = Math.sin(latitudeDistance / 2) * Math.sin(latitudeDistance / 2)
+        double latDistance = Math.toRadians(classroomLatitude - studentLatitude);
+        double lonDistance = Math.toRadians(classroomLongitude - studentLongitude);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
                 + Math.cos(Math.toRadians(studentLatitude)) * Math.cos(Math.toRadians(classroomLatitude))
-                * Math.sin(longitudeDistance / 2) * Math.sin(longitudeDistance / 2);
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = earthRadius * c;
+        double distance = earthRadius * c; // convert to meters
 
         distance = Math.pow(distance, 2);
-        if (distance < Math.sqrt(200)){
+
+        if (Math.sqrt(distance) < Math.sqrt(200)){
             return true;
         }
         else {
