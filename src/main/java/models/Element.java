809cc621dffab425286184c0bc17fcc962ebf2ca
Veimar Choque <veimar.choque@truextend.com>
2020-07-24 20:52:42 -0400

    [develop] Create model to manage students and classrooms

--- src/main/java/models/Element.java
+++ src/main/java/models/Element.java
@@ -1,12 +1,12 @@
 package models;
 
-public class Student {
+public class Element {
 
     private String name;
     private double latitude;
     private double longitude;
 
-    public Student(String name, double latitude, double longitude) {
+    public Element(String name, double latitude, double longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
