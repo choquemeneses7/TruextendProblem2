c04579dcff2ec3ff48afde1b7ceeb0bc38ef346b
Veimar Choque <veimar.choque@truextend.com>
2020-07-29 14:03:41 -0400

    [develop]: Refactor objects based in the new entities created, clean code naming conventions

--- src/main/java/functions/StudentsClassroomVerification.java
+++ src/main/java/functions/StudentsClassroomVerification.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public class StudentsClassroomVerification {
 
-    public static List<Student> studentsInClasses(List<Student> studentsList, List<Classroom> classroomsList){
+    public static List<Student> studentsInClassroom(List<Student> studentsList, List<Classroom> classroomsList){
         List<Student> studentsInClass = new ArrayList<Student>();
         for(Student student : studentsList) {
             for(Classroom classroom : classroomsList) {
-                if(isStudentInClass(student, classroom)) {
+                if(isStudentInClassroom(student, classroom)) {
                     studentsInClass.add(student);
                     break;
                 }
@@ -21,7 +21,7 @@ public class StudentsClassroomVerification {
         return studentsInClass;
     }
 
-    private static boolean isStudentInClass(Student student, Classroom classroom) {
+    private static boolean isStudentInClassroom(Student student, Classroom classroom) {
         double studentLatitude = student.getLatitude();
         double studentLongitude = student.getLongitude();
         double classroomLatitude = classroom.getLatitude();
@@ -39,12 +39,11 @@ public class StudentsClassroomVerification {
         double positionStudent = 2 * Math.atan2(Math.sqrt(positionClassroom), Math.sqrt(1 - positionClassroom));
         double distance = earthRadius * positionStudent;
 
-        if (distance < Math.sqrt(classroomMaxLength)){
+        if (distance < classroomMaxLength){
             return true;
         }
         else {
             return false;
         }
-
     }
 }
--- src/test/java/StudentsClassroomVerificationTests.java
+++ src/test/java/StudentsClassroomVerificationTests.java
@@ -1,3 +1,4 @@
+import models.Classroom;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
@@ -8,21 +9,21 @@ import models.Student;
 public class StudentsClassroomVerificationTests {
 
     @Test
-    public void testCase1() {
+    public void testThreeStudentsInClassrooms() {
         List<Student> studentList = new ArrayList<Student>();
-        List<Student> classroomList = new ArrayList<Student>();
+        List<Classroom> classroomList = new ArrayList<Classroom>();
         Student johnStudent = new Student("John Wilson", 34.069149, -118.442639);
         Student janeStudent = new Student("Jane Graham", 34.069601, -118.441862);
         Student pamStudent = new Student("Pam Bam", 34.071513, -118.441181);
         studentList.add(johnStudent);
         studentList.add(janeStudent);
         studentList.add(pamStudent);
-        classroomList.add(new Student("Principles of computational geo-location analysis", 34.069140, -118.442689));
-        classroomList.add(new Student("Sedimentary Petrology", 34.069585, -118.441878));
-        classroomList.add(new Student("Introductory Psychobiology", 34.069742, -118.441312));
-        classroomList.add(new Student("Art of Listening", 34.070223, -118.440193));
-        classroomList.add(new Student("Art Hitory", 34.071528, -118.441211));
-        List<Student> studentFoundList = StudentsClassroomVerification.studentsInClasses(studentList, classroomList);
+        classroomList.add(new Classroom("Principles of computational geo-location analysis", 34.069140, -118.442689));
+        classroomList.add(new Classroom("Sedimentary Petrology", 34.069585, -118.441878));
+        classroomList.add(new Classroom("Introductory Psychobiology", 34.069742, -118.441312));
+        classroomList.add(new Classroom("Art of Listening", 34.070223, -118.440193));
+        classroomList.add(new Classroom("Art Hitory", 34.071528, -118.441211));
+        List<Student> studentFoundList = StudentsClassroomVerification.studentsInClassroom(studentList, classroomList);
         List<Student> expectedStudentFoundList = new ArrayList<Student>();
         expectedStudentFoundList.add(johnStudent);
         expectedStudentFoundList.add(janeStudent);
@@ -31,42 +32,42 @@ public class StudentsClassroomVerificationTests {
     }
 
     @Test
-    public void testCase2() {
+    public void testOneStudentInClassrooms() {
         List<Student> studentList = new ArrayList<Student>();
-        List<Student> classroomList = new ArrayList<Student>();
+        List<Classroom> classroomList = new ArrayList<Classroom>();
         Student johnStudent = new Student("John Wilson", 34.069849, -118.443539);
         Student janeStudent = new Student("Jane Graham", 34.069901, -118.441562);
         Student pamStudent = new Student("Pam Bam", 34.071523, -118.441171);
         studentList.add(johnStudent);
         studentList.add(janeStudent);
         studentList.add(pamStudent);
-        classroomList.add(new Student("Principles of computational geo-location analysis", 33.05, -118.442689));
-        classroomList.add(new Student("Sedimentary Petrology", 34.069585, -118.441878));
-        classroomList.add(new Student("Introductory Psychobiology", 34.069742, -118.441312));
-        classroomList.add(new Student("Art of Listening", 34.070223, -118.440193));
-        classroomList.add(new Student("Art Hitory", 34.071528, -118.441211));
-        List<Student> studentFoundList = StudentsClassroomVerification.studentsInClasses(studentList, classroomList);
+        classroomList.add(new Classroom("Principles of computational geo-location analysis", 33.05, -118.442689));
+        classroomList.add(new Classroom("Sedimentary Petrology", 34.069585, -118.441878));
+        classroomList.add(new Classroom("Introductory Psychobiology", 34.069742, -118.441312));
+        classroomList.add(new Classroom("Art of Listening", 34.070223, -118.440193));
+        classroomList.add(new Classroom("Art Hitory", 34.071528, -118.441211));
+        List<Student> studentFoundList = StudentsClassroomVerification.studentsInClassroom(studentList, classroomList);
         List<Student> expectedStudentFoundList = new ArrayList<Student>();
         expectedStudentFoundList.add(pamStudent);
         assertEquals(studentFoundList,expectedStudentFoundList);
     }
 
     @Test
-    public void testStudentsAreNotInClasses() {
+    public void testStudentsAreNotInClassrooms() {
         List<Student> studentList = new ArrayList<Student>();
-        List<Student> classroomList = new ArrayList<Student>();
+        List<Classroom> classroomList = new ArrayList<Classroom>();
         Student johnStudent = new Student("John Wilson", 34.069849, -118.443539);
         Student janeStudent = new Student("Jane Graham", 34.069901, -118.441562);
         Student pamStudent = new Student("Pam Bam", 34.071633, -118.441171);
         studentList.add(johnStudent);
         studentList.add(janeStudent);
         studentList.add(pamStudent);
-        classroomList.add(new Student("Principles of computational geo-location analysis", 34.0668, -118.4435));
-        classroomList.add(new Student("Sedimentary Petrology", 34.069585, -118.441878));
-        classroomList.add(new Student("Introductory Psychobiology", 34.069748, -118.441312));
-        classroomList.add(new Student("Art of Listening", 34.070223, -118.440193));
-        classroomList.add(new Student("Art Hitory", 34.071528, -118.4413));
-        List<Student> studentFoundList = StudentsClassroomVerification.studentsInClasses(studentList, classroomList);
+        classroomList.add(new Classroom("Principles of computational geo-location analysis", 34.0668, -118.4435));
+        classroomList.add(new Classroom("Sedimentary Petrology", 34.069585, -118.441878));
+        classroomList.add(new Classroom("Introductory Psychobiology", 34.069748, -118.441312));
+        classroomList.add(new Classroom("Art of Listening", 34.070223, -118.440193));
+        classroomList.add(new Classroom("Art Hitory", 34.071528, -118.4413));
+        List<Student> studentFoundList = StudentsClassroomVerification.studentsInClassroom(studentList, classroomList);
         List<Student> expectedStudentFoundList = new ArrayList<Student>();
         assertEquals(studentFoundList,expectedStudentFoundList);
     }
